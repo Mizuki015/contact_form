6d416dda11bd9fd85d75f8e31b2c019c5f0507be
城間海月 <mizushiro426426@gmail.com>
2019-08-29 13:12:22 +0800

    入力してないと怒られるコードが書かれた

--- check.php
+++ check.php
@@ -2,7 +2,28 @@
 //スーパーグローバル変数
 //（phpがもともと用意している変数）
 
-var_dump($_POST);
+$username = $_POST['username'];
+$email = $_POST['email'];
+$content = $_POST['content'];
+
+// ユーザー名が空かチェック
+if ($username == '') {
+    $usernameResult = 'なんで入力しないんですか？';
+} else {
+    $usernameResult = $username;
+}
+
+if ($email == '') {
+    $emailResult = 'なんで入力しないんですか？';
+} else {
+    $emailResult = $email;
+}
+
+if ($content == '') {
+    $contentResult = 'なんで入力しないんですか？';
+} else {
+    $contentResult = $content;
+}
 
 ?>
 <!DOCTYPE html>
@@ -16,9 +37,9 @@ var_dump($_POST);
 <body>
     <h1>入力内容確認</h1>
 
-    <p>名前：<?php echo $_POST['username']; ?></p>
-    <p>メールアドレス：<?php echo $_POST['email']; ?></p>
-    <p>お問合わせ内容：<?php echo $_POST['content']; ?></p>
+    <p>名前：<?php echo $usernameResult; ?></p>
+    <p>メールアドレス：<?php echo $emailResult; ?></p>
+    <p>お問合わせ内容：<?php echo $contentResult; ?></p>
 
     <form action="">
         <button>戻る</button>
